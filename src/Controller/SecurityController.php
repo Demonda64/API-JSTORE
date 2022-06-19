0de17cb69b4ac165fb347d34ca7b162cbcefe8ef
Demonda64 <oscardemonda@outlokk.fr>
2022-06-20 01:27:58 +0200

    Fin du blog détail à revoir

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -57,5 +57,11 @@ class SecurityController extends AbstractController
 
             return $this->render('security/login.html.twig');
     }
+        /**
+         * @Route("/logout", name="security_logout")
+         */
+        public function logout(): Response{
 
+            
+    }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,14 +7,18 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
-//use Symfony\Component\Security\Core\User;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\Table(name="`user`")
+ * @UniqueEntity(
+ * fields = {"email"},
+ * message = "Cet email existe déja"
+ * )
  */
 class User implements UserInterface , PasswordAuthenticatedUserInterface
 {
